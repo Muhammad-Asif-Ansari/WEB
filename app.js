6f4347f90609ca0d65db27db4a702453ab963375
Muhammad Asif Ansari <muhammadasifansari101@gmail.com>
2024-09-20 12:12:31 +0500

    ASSIGNMETNT 01 & 02
    ASSIGNMETNT 01 & 02

---
+++ app.js
@@ -0,0 +1,60 @@
+// ASSIGNMENT 01
+
+// QUESTION 01:
+// alert("WELCOME MY WEBSITE!");
+
+//QUESTION 02:
+//alert("Error! Please enter a valid password");
+
+//QUESTION 03:
+//alert ("Welcome to JS Land...\n Happy Coding!");
+
+//QUESTION 03:
+//alert ("Welcome to JS Land...");
+
+//QUESTION 04:
+//alert ("Happy Coding! \n Prevent this page from creating additional diaglogs. ");
+
+//QUESTION 05:
+//alert ("Hello... I can run JS through my web browser's console");
+
+
+//ASSIGNMENT 02
+
+// QUESTION 01:
+var name1 = "Jhone Doe";
+alert(name1);
+
+//QUESTION 02:
+var name2 = "15 years old";
+alert(name2);
+
+//QUESTION 03:
+var name3 = "Certified Mobile Application Development";
+alert(name3);
+
+//QUESTION 04:
+var name4 = "PIZZA\n PIZZ \n PIZ \n PI\n P";
+alert(name4);
+
+//QUESTION 05:
+var name5 = "My email address is muhammadasifansari101@gmail.com";
+alert(name5);
+
+//QUESTION 06:
+var name6 = "I am trying to learn from the Book a smarter\n way to learn Javascript";
+alert(name6);
+
+//QUESTION 07:
+var userName = "BILAL";
+
+//QUESTION 08 :
+var myName = MuhammadAsif;
+var fullName = "Muhammad Asif Ansari";
+
+//QUESTION 09:
+
+
+
+
+
