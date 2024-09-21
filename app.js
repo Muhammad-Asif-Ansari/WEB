8c65262409965c3ee177148b2bd75bc900f31f5b
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-09-21 20:08:07 +0500

    Javascript chapter 1 to 3

--- app.js
+++ app.js
@@ -22,37 +22,62 @@
 //ASSIGNMENT 02
 
 // QUESTION 01:
-var name1 = "Jhone Doe";
-alert(name1);
+//var name1 = "Jhone Doe";
+//alert(name1);
 
 //QUESTION 02:
-var name2 = "15 years old";
-alert(name2);
+//var name2 = "15 years old";
+//alert(name2);
 
 //QUESTION 03:
-var name3 = "Certified Mobile Application Development";
-alert(name3);
+//var name3 = "Certified Mobile Application Development";
+//alert(name3);
 
 //QUESTION 04:
-var name4 = "PIZZA\n PIZZ \n PIZ \n PI\n P";
-alert(name4);
+//var name4 = "PIZZA\n PIZZ \n PIZ \n PI\n P";
+//alert(name4);
 
 //QUESTION 05:
-var name5 = "My email address is muhammadasifansari101@gmail.com";
-alert(name5);
+//var name5 = "My email address is muhammadasifansari101@gmail.com";
+//alert(name5);
 
 //QUESTION 06:
-var name6 = "I am trying to learn from the Book a smarter\n way to learn Javascript";
-alert(name6);
+//var name6 = "I am trying to learn from the Book a smarter\n way to learn Javascript";
+//alert(name6);
 
 //QUESTION 07:
-var userName = "BILAL";
+//var userName = "BILAL";
 
 //QUESTION 08 :
-var myName = MuhammadAsif;
-var fullName = "Muhammad Asif Ansari";
+//var myName = MuhammadAsif;
+//var fullName = "Muhammad Asif Ansari";
 
 //QUESTION 09:
+//alert("Yah! I can write HTML content through Javascript");
+
+
+
+//ASSIGNMENT 03
+//QUESTION 01
+//var age = 15;
+//alert(age);
+
+//QUESTION 02
+//var visitor = 14;
+//alert(visitor);
+
+//QUESTION 03
+//var birthYear = 2002;
+//alert(birthYear);
+
+
+//QUESTION 04
+//var vistorName = "John Doe\n";
+//var productTitle = "XYZ Clothing Store\n";
+//var quantity = "5 T-shirt";
+//alert(vistorName+productTitle+quantity);
+
+
 
 
 
